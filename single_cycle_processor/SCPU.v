72a8be3578c60606954c2800f112cf40aead4535
hz826 <2945994284@qq.com>
2022-06-19 19:04:04 +0800

    整理了单周期CPU的代码

--- single_cycle_processor/SCPU.v
+++ single_cycle_processor/SCPU.v
@@ -6,21 +6,21 @@
 `include "ALU.v"
 
 module SCPU(
-    input      clk,            // clock
-    input      reset,          // reset
+    input      clk,           // clock
+    input      reset,         // reset
+    input  [4:0]  reg_sel,    // register selection     (for debug use)
+    output [31:0] reg_data,   // selected register data (for debug use)
+
+    // IM
+    output [31:0] PC_out,      // PC address
     input [31:0]  inst_in,     // instruction
-    input [31:0]  Data_in,     // data from data memory
    
+   // DM
     output    mem_w,          // output: memory write signal
-    output [31:0] PC_out,     // PC address
-      // memory write
     output [31:0] Addr_out,   // ALU output
     output [31:0] Data_out,   // data to data memory
-
-    input  [4:0] reg_sel,    // register selection (for debug use)
-    output [31:0] reg_data,  // selected register data (for debug use)
-    
-    output [2:0] DMType
+    output [2:0] DMType,      // read/write data length
+    input [31:0]  Data_in     // data from data memory
 );
 
     wire        RegWrite;    // control signal to register write
@@ -46,19 +46,21 @@ module SCPU(
     wire [31:0] Imm32;       // 32-bit immediate
     wire [19:0] IMM;         // 20-bit immediate (address)
     wire [4:0]  A3;          // register address for write
-    reg [31:0] WD;           // register write data
+    reg  [31:0] WD;          // register write data
     wire [31:0] RD1,RD2;     // register data specified by rs
-    wire [31:0] B;           // operator for ALU B
+    wire [31:0] ALU_B;       // operator for ALU B
 	
-	wire [4:0] iimm_shamt;
+	wire [4:0]  iimm_shamt;
 	wire [11:0] iimm,simm,bimm;
 	wire [19:0] uimm,jimm;
 	wire [31:0] immout;
-    wire[31:0] aluout;
+    wire [31:0] aluout;
+
     assign Addr_out=aluout;
-	assign B = (ALUSrc) ? immout : RD2;
+	assign ALU_B = (ALUSrc) ? immout : RD2;
 	assign Data_out = RD2;
 	
+    /*** 指令处理 ***/
 	assign iimm_shamt=inst_in[24:20];
 	assign iimm=inst_in[31:20];
 	assign simm={inst_in[31:25],inst_in[11:7]};
@@ -66,23 +68,24 @@ module SCPU(
 	assign uimm=inst_in[31:12];
 	assign jimm={inst_in[31],inst_in[19:12],inst_in[20],inst_in[30:21]};
    
-    assign Op = inst_in[6:0];  // instruction
+    assign Op = inst_in[6:0];       // instruction
     assign Funct7 = inst_in[31:25]; // funct7
     assign Funct3 = inst_in[14:12]; // funct3
-    assign rs1 = inst_in[19:15];  // rs1
-    assign rs2 = inst_in[24:20];  // rs2
-    assign rd = inst_in[11:7];  // rd
-    assign Imm12 = inst_in[31:20];// 12-bit immediate
-    assign IMM = inst_in[31:12];  // 20-bit immediate
+    assign rs1 = inst_in[19:15];    // rs1
+    assign rs2 = inst_in[24:20];    // rs2
+    assign rd = inst_in[11:7];      // rd
+    assign Imm12 = inst_in[31:20];  // 12-bit immediate
+    assign IMM = inst_in[31:12];    // 20-bit immediate
+    /*** 指令处理 ***/
    
-   // instantiation of control unit
+    // instantiation of control unit
 	ctrl U_ctrl(
 		.Op(Op), .Funct7(Funct7), .Funct3(Funct3), .Zero(Zero), 
 		.RegWrite(RegWrite), .MemWrite(mem_w),
 		.EXTOp(EXTOp), .ALUOp(ALUOp), .NPCOp(NPCOp), 
 		.ALUSrc(ALUSrc), .GPRSel(GPRSel), .WDSel(WDSel), .DMType(DMType)
 	);
- // instantiation of pc unit
+    // instantiation of pc unit
 	PC U_PC(.clk(clk), .rst(reset), .NPC(NPC), .PC(PC_out) );
 	NPC U_NPC(.PC(PC_out), .NPCOp(NPCOp), .IMM(immout), .NPC(NPC), .aluout(aluout));
 	EXT U_EXT(
@@ -99,18 +102,16 @@ module SCPU(
 		//.reg_sel(reg_sel),
 		//.reg_data(reg_data)
 	);
-// instantiation of alu unit
-	alu U_alu(.A(RD1), .B(B), .ALUOp(ALUOp), .C(aluout), .Zero(Zero), .PC(PC_out));
+    // instantiation of alu unit
+	alu U_alu(.A(RD1), .B(ALU_B), .ALUOp(ALUOp), .C(aluout), .Zero(Zero), .PC(PC_out));
 
-//please connnect the CPU by yourself
-always @*
-begin
-	case(WDSel)
-		`WDSel_FromALU: WD<=aluout;
-		`WDSel_FromMEM: WD<=Data_in;
-		`WDSel_FromPC: WD<=PC_out+4;
-	endcase
-end
-
-
+    //please connnect the CPU by yourself
+    always @*
+    begin
+        case(WDSel)
+            `WDSel_FromALU: WD<=aluout;
+            `WDSel_FromMEM: WD<=Data_in;
+            `WDSel_FromPC: WD<=PC_out+4;
+        endcase
+    end
 endmodule
